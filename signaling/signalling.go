37b7437182161c0170eac2cb47a598a1a114ac31
Emmanuel <144928280+EmmanuelAbolade@users.noreply.github.com>
2025-10-27 04:03:17 +0000

    Updated signalling lab code

--- signaling/signalling.go
+++ signaling/signalling.go
@@ -5,37 +5,39 @@ import (
 	"sync"
 	"time"
 )
-//Global variables shared between functions --A BAD IDEA
 
-
-
-
+//No Global variables shared between functions --A good IDEA
 
 func main() {
 	var wg sync.WaitGroup
+	// Unbuffered channel for synchronization
 	barrier := make(chan bool)
 
-	doStuffOne:= func() bool {
+	doStuffOne := func() {
+		defer wg.Done()
 		fmt.Println("StuffOne - Part A")
-		//wait here
+		//wait here (Send signal - will block until doStuffTwo receives)
 		barrier <- true
 		fmt.Println("StuffOne - PartB")
-		wg.Done()
-		return true
+
 	}
-	doStuffTwo := func () bool {
-		time.Sleep(time.Second*5)
+	doStuffTwo := func() {
+		defer wg.Done()
+
+		// Simulate some work
+		time.Sleep(time.Second * 5)
 		fmt.Println("StuffTwo - Part A")
-		//wait here
-	
+
+		//wait here (Receive signal - unblocks doStuffOne_
 		<-barrier
 		fmt.Println("StuffTwo - PartB")
-		wg.Done()
-		return true
+
 	}
 	wg.Add(2)
 	go doStuffOne()
 	go doStuffTwo()
-	wg.Wait() //wait here until everyone (10 go routines) is done
-	
+	wg.Wait()      //wait here until everyone (10 go routines) is done
+	close(barrier) // Clean up the channel
+	fmt.Println("All done!")
+
 }
