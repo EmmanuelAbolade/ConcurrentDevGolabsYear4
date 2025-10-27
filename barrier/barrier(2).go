9fa7c389314f8524665c6b8ac2a95b2473911c0f
Emmanuel <144928280+EmmanuelAbolade@users.noreply.github.com>
2025-10-27 06:21:19 +0000

    Updated barrier implementation using mutex and semaphore lab code

--- barrier/barrier(2).go
+++ barrier/barrier(2).go
@@ -1,6 +1,5 @@
-
 //Barrier.go Template Code
-//Copyright (C) 2024 Dr. Joseph Kehoe
+//Copyright (C) 2025 Emmanuel Abolade
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -15,13 +14,11 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 //--------------------------------------------
-// Author: Joseph Kehoe (Joseph.Kehoe@setu.ie)
-// Created on 30/9/2024
+// Author: Emmanuel Abolade (c00288657@setu.ie)
+// Created on 19/10/2024
 // Modified by:
 // Issues:
-// The barrier is not implemented!
 //--------------------------------------------
 
 package main
@@ -31,36 +28,63 @@ import (
 	"fmt"
 	"sync"
 	"time"
+
 	"golang.org/x/sync/semaphore"
 )
 
+// Barrier implementation using Mutex and Semaphores
+func doSomeStuff(goNum int, wg *sync.WaitGroup, mutex *sync.Mutex, counter *int,
+	sem *semaphore.Weighted, ctx context.Context, totalRoutines int) bool {
+	defer wg.Done()
 
-// Place a barrier in this function --use Mutex's and Semaphores
-func doStuff(goNum int, wg *sync.WaitGroup) bool {
 	time.Sleep(time.Second)
-	fmt.Println("Part A",goNum)
-	//we wait here until everyone has completed part A
-	fmt.Println("PartB",goNum)
-	wg.Done()
+	fmt.Println("Part A", goNum)
+
+	//Barrier: we wait here until everyone has completed part A
+	mutex.Lock()
+	*counter++
+	if *counter == totalRoutines {
+		// Last one to arrive - release everyone
+		sem.Release(int64(totalRoutines))
+	}
+	mutex.Unlock()
+
+	// Wait at barrier
+	if err := sem.Acquire(ctx, 1); err != nil {
+		fmt.Printf("Error acquiring semaphore: %v\n", err)
+		return false
+	}
+	fmt.Println("Part B", goNum)
+
 	return true
 }
 
-
 func main() {
-	totalRoutines:=10
+	totalRoutines := 10
 	var wg sync.WaitGroup
-	wg.Add(totalRoutines)
+	var mutex sync.Mutex
+	var counter = 0
+
 	//we will need some of these
 	ctx := context.TODO()
-	var theLock sync.Mutex
+	//var theLock sync.Mutex
 	sem := semaphore.NewWeighted(int64(totalRoutines))
-	theLock.Lock()
-	sem.Acquire(ctx, 1)
-	for i := range totalRoutines {//create the go Routines here
-		go doStuff(i, &wg)
+
+	// Acquire all semaphore slots initially (barrier is closed)
+	err := sem.Acquire(ctx, int64(totalRoutines))
+	if err != nil {
+		return
 	}
-	sem.Release(1)
-	theLock.Unlock()
-	
+
+	//theLock.Lock()
+	//sem.Acquire(ctx, 1)
+	wg.Add(totalRoutines)
+
+	for i := range totalRoutines { //create the go Routines here
+		go doSomeStuff(i, &wg, &mutex, &counter, sem, ctx, totalRoutines)
+	}
+	//sem.Release(1)
+	//theLock.Unlock()
+
 	wg.Wait() //wait for everyone to finish before exiting
 }
