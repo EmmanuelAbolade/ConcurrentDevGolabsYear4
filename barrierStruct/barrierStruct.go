97f5be7821d45c39f74e4545e6c4f3f7b592dc52
Emmanuel <144928280+EmmanuelAbolade@users.noreply.github.com>
2025-10-27 15:03:42 +0000

    Updated barrierStruct lab code to prevent deadlock

--- barrierStruct/barrierStruct.go
+++ barrierStruct/barrierStruct.go
@@ -1,73 +1,74 @@
 package main
 
 import (
-     "fmt"
-     "sync"
-     "time"
-     "math/rand/v2"
+	"fmt"
+	"math/rand/v2"
+	"sync"
+	"time"
 )
 
-
-//Create a barrier data type
-type barrier struct{
-     theChan chan bool
-     theLock sync.Mutex
-     total int
-     count int
+// Create a barrier data type
+type barrier struct {
+	theChan chan bool
+	theLock sync.Mutex
+	total   int
+	count   int
 }
 
-//creates a properly initialised barrier
-//N== number of threads (go Routines)
-func createBarrier(N int) barrier{
-     theBarrier:= barrier{
-         theChan : make(chan bool),
-         total: N,
-         count : 0,
-     }
-     return theBarrier
+// creates a properly initialised barrier
+// N== number of threads (go Routines)
+func createBarrier(N int) *barrier {
+	theBarrier := barrier{
+		theChan: make(chan bool),
+		total:   N,
+		count:   0,
+	}
+	return &theBarrier
 }
 
-//Method belonging to barrier data type
-//Blocks until everyone reaches this point then lets everyone continue
-func (b *barrier) wait(){
-     b.theLock.Lock()
-     b.count++
-     if b.count== b.total{
-         b.theLock.Unlock()
-         fmt.Println("here")
-         for _= range b.total-1{
-             <- b.theChan
-         }
-     }else{
-         fmt.Println(b.count)
-         b.theLock.Unlock()
-         b.theChan <- true
-     }
-}//wait
+// Method belonging to barrier data type
+// Blocks until everyone reaches this point then lets everyone continue
+func (b *barrier) wait() {
+	b.theLock.Lock()
+	b.count++
+	if b.count == b.total {
+		b.theLock.Unlock()
+		fmt.Println("All here! Releasing...")
+
+		for i := 0; i < b.total-1; i++ {
+			b.theChan <- true // FIXED: Send (not receive)
+		}
+	} else {
+		fmt.Println(b.count)
+
+		b.theLock.Unlock()
+		//b.theChan <- true
+		<-b.theChan
+	}
+} //wait
 
 func WorkWithRendezvous(wg *sync.WaitGroup, Num int, theBarrier *barrier) bool {
-     var X time.Duration
-     X=time.Duration(rand.IntN(5))
-     time.Sleep(X * time.Second)//wait random time amount
-     fmt.Println("Part A", Num)
-     //Rendezvous here
-     theBarrier.wait()
-     fmt.Println("PartB",Num)
-     wg.Done()
-     return true
+	var X time.Duration
+	X = time.Duration(rand.IntN(5))
+	// Random sleep (0-4 seconds)
+	time.Sleep(X * time.Second) //wait random time amount
+	fmt.Println("Part A", Num)
+	//Rendezvous here
+	theBarrier.wait()
+	fmt.Println("Part B", Num)
+	wg.Done()
+	return true
 }
 
-
-
 func main() {
-     var wg sync.WaitGroup
-     barrier := createBarrier(5)
-     threadCount:=5
+	var wg sync.WaitGroup
+	barrier := createBarrier(5)
+	threadCount := 5
 
-     wg.Add(threadCount)
-     for N := range threadCount {
-         go WorkWithRendezvous(&wg, N,&barrier)
-     }
-     wg.Wait() //wait here until everyone (5 go routines) is done
+	wg.Add(threadCount)
+	for N := range threadCount {
+		go WorkWithRendezvous(&wg, N, barrier)
+	}
+	wg.Wait() //wait here until everyone (5 go routines) is done
 
 }
